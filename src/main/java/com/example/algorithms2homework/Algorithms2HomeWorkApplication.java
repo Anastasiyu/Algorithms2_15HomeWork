48273fcfa1d09f9160b19965608862f826255992
Anastasiyu <asy.safiulina@mail.ru>
2022-10-23 18:39:39 +0300

    Initial commit

---
+++ src/main/java/com/example/algorithms2homework/Algorithms2HomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithms2homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithms2HomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithms2HomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithms2homework/Algorithms2HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithms2homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithms2HomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
